33d384ea3f9d72adc5a9a06b25254ce1edc7ddec
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2024-08-06 16:35:24 +0700

    create contact widget

---
+++ lib/contact_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ContactWidget extends StatefulWidget {
+  const ContactWidget({Key? key}) : super(key: key);
+
+  @override
+  _ContactWidgetState createState() => _ContactWidgetState();
+}
+
+class _ContactWidgetState extends State<ContactWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("My Cotact"),
+        ),
+        body: Center(
+          child: Text('MyContact'),
+        ));
+  }
+}
