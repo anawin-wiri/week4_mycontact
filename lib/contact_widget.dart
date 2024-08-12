b8eac68e1ce0e83f05f403e845ab9c8491376aea
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2024-08-12 21:13:13 +0700

    add ontap

--- lib/contact_widget.dart
+++ lib/contact_widget.dart
@@ -144,90 +144,101 @@ class ContactListView2 extends StatelessWidget {
           ),
           itemCount: contactProvider.items.length,
           itemBuilder: (context, index) {
-            return Card(
-              color: const Color.fromARGB(255, 252, 251, 237),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              elevation: 8,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  // Changed from Row to Column for better alignment in GridView
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        // Leading: Circle Avatar with the first letter of the name
-                        CircleAvatar(
-                          child: Text(contactProvider.items[index].name[0]),
-                        ),
-                        SizedBox(
-                            width: 16.0), // Spacing between avatar and text
-
-                        // Title and Subtitle
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                contactProvider.items[index].name,
-                                style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(
-                                  height:
-                                      4.0), // Spacing between name and details
-                              Text(contactProvider.items[index].phone),
-                              Text(contactProvider.items[index].email),
-                            ],
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    final provider =
+                        Provider.of<ContactProvider>(context, listen: false);
+                    provider.setCurrentContact(provider.items[index]);
+                    return EditContactWidget(); // Navigate to the EditContactWidget or any desired screen
+                  },
+                ));
+              },
+              child: Card(
+                color: const Color.fromARGB(255, 252, 251, 237),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                elevation: 8,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          // Leading: Circle Avatar with the first letter of the name
+                          CircleAvatar(
+                            child: Text(contactProvider.items[index].name[0]),
                           ),
-                        ),
-                      ],
-                    ),
+                          SizedBox(
+                              width: 16.0), // Spacing between avatar and text
 
-                    // Trailing: Delete Icon Button
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('Delete Contact'),
-                                content: Text(
-                                    'Are you sure you want to delete this contact?'),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text('Cancel'),
+                          // Title and Subtitle
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  contactProvider.items[index].name,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  TextButton(
-                                    onPressed: () {
-                                      Provider.of<ContactProvider>(context,
-                                              listen: false)
-                                          .deleteContact(index);
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text('Delete'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
+                                ),
+                                SizedBox(
+                                    height:
+                                        4.0), // Spacing between name and details
+                                Text(contactProvider.items[index].phone),
+                                Text(contactProvider.items[index].email),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+
+                      // Trailing: Delete Icon Button
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text('Delete Contact'),
+                                  content: Text(
+                                      'Are you sure you want to delete this contact?'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text('Cancel'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        Provider.of<ContactProvider>(context,
+                                                listen: false)
+                                            .deleteContact(index);
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text('Delete'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
